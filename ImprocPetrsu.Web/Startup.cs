b0477c0765a9b41f8fe1bceb4c52036f992c49e2
Denis Karpovskiy <geext29@gmail.com>
2021-02-16 03:25:07 +0300

    Start swagger in production

--- ImprocPetrsu.Web/Startup.cs
+++ ImprocPetrsu.Web/Startup.cs
@@ -51,9 +51,10 @@ namespace ImprocPetrsu.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ImprocPetrsu.Web v1"));
             }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ImprocPetrsu.Web v1"));
 
             app.UseHttpsRedirection();
 
